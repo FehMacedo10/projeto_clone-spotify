21fb5cdfd4b945de1e28029c955fe1e1838e429f
FehMacedo10 <fmacedoribeiro@outlook.com>
2025-02-18 15:25:41 -0300

    ✨ feat: Ajustes para o Deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,11 @@
 // Fetch ou Axios
 // Fetch -> nativo do browser
 // Axios -> biblioteca de terceiros
-import axios from 'axios';
+import "dotenv/config";
+import axios from "axios";
 
-const URL = "http://localhost:3000"
+const NODE_ENV = process.env.NODE_ENV; // development ou production
+const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api"; //
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
